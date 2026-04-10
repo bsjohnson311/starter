c8d7c94ddfa90eb104651180a9084e4e7c9007ac
Bryce <bsjohnson311@gmail.com>
2026-04-10 11:03:45 -0700

    Update typst-preview.lua

--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -1,9 +1,14 @@
 return {
   {
     "chomosuke/typst-preview.nvim",
-    lazy = false,
+    ft = "typst",
     version = "1.*",
-    opts = {},
+    build = function()
+      require("typst-preview").update()
+    end,
+    opts = {
+      open_cmd = (vim.fn.executable("google-chrome") == 1 and "google-chrome %s" or "chromium %s"),
+    },
     keys = {
       { "<leader>tp", "<cmd>TypstPreviewToggle<CR>", desc = "Toggle Typst Preview" },
     },
