1e0cf45adc3e3ab4cebfc37ead936a9bc3c5e8df
bryce <bsjohnson311@gmail.com>
2026-04-09 21:56:50 -0700

    Added keymap for tabular

--- lua/plugins/tabular.lua
+++ lua/plugins/tabular.lua
@@ -1,3 +1,21 @@
 return {
-  {'godlygeek/tabular', lazy = false, enabled = true},
+  {
+    'godlygeek/tabular',
+    lazy = false,
+    enabled = true,
+    keys = {
+      {
+        "<leader>tb",
+        function()
+          vim.ui.input({ prompt = "Align pattern: " }, function(input)
+            if input then
+              -- Match only first instance per line: from start, skip non-matching chars, then align on pattern
+              vim.cmd("Tab /^[^" .. input .. "]*\\zs" .. input)
+            end
+          end)
+        end,
+        desc = "Tabular align",
+      },
+    },
+  },
 }
