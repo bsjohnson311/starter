b402a6027d3abdb89399aeadaa923c9e43d81b62
bryce <bsjohnson311@gmail.com>
2026-04-09 22:20:58 -0700

    Added TabFirst command

--- lua/plugins/tabular.lua
+++ lua/plugins/tabular.lua
@@ -3,14 +3,20 @@ return {
     'godlygeek/tabular',
     lazy = false,
     enabled = true,
+    init = function()
+      -- :TabFirst <pattern> aligns only the first instance per line
+      vim.api.nvim_create_user_command("TabFirst", function(opts)
+        local pattern = opts.args
+        vim.cmd("Tab /^[^" .. pattern .. "]*\\zs" .. pattern)
+      end, { nargs = 1 })
+    end,
     keys = {
       {
         "<leader>tb",
         function()
           vim.ui.input({ prompt = "Align pattern: " }, function(input)
             if input then
-              -- Match only first instance per line: from start, skip non-matching chars, then align on pattern
-              vim.cmd("Tab /^[^" .. input .. "]*\\zs" .. input)
+              vim.cmd("TabFirst " .. input)
             end
           end)
         end,
